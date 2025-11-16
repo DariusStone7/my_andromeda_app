b0c887c2f374320fbb4dbcaf3b8a5beceef2f4b5
dariusstone7 <stonefightx@gmail.com>
2025-11-16 14:28:07 +0100

    home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,21 @@
 class HomeController < ApplicationController
   def index
+    @images = Image.all
+    @image = Image.new
+  end
+
+  def create
+    @image = Image.new(image_params)
+    if @image.save
+      redirect_to root_path, notice: "Image upload sucessful!"
+    else
+      redirect_to root_path, alert: "Image upload error."
+    end
+  end
+
+  private
+
+  def image_params
+    params.require(:image).permit(:file)
   end
 end
